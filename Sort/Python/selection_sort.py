d6dfd0d742853745cb2ea3e68c760d6bc50967b1
Jake <imjakeeyy@gmail.com>
2020-01-28 18:43:39 +0100

    Started on sorting algos

---
+++ Sort/Python/selection_sort.py
@@ -0,0 +1,23 @@
+#Selectionsort pretty is slow, time complexity is O(N^2)
+from randomGen import randomArray
+from randomGen import numberToSeach
+import math
+
+def selection_sort(length):
+    for i in range(length):
+
+        #search for a smaller element in the array.
+        min_index = i
+        for j in range(i+1, length):
+            if arrToSearch[min_index] > arrToSearch[j]:
+                min_index = j
+                
+        temp = arrToSearch[min_index]
+        arrToSearch[min_index] = arrToSearch[i]
+        arrToSearch[i] = temp 
+
+
+arrToSearch = randomArray()
+selection_sort(len(arrToSearch))
+
+print(arrToSearch)
